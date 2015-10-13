9d79643851092f40cba2aa04f203aeae9c901320
Jasand <jasand@vermilion.com>
2015-10-12 22:44:57 -0600

    styling, time records working

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,7 +1,27 @@
 (function($, angular, App, window) {
   'use strict';
   
+
+ 
   var pressurizeApp = angular.module('pressurizeApp', ['ngRoute']);
+  
+  pressurizeApp.factory('uuid', function() {
+    var svc = {
+      new: function() {
+        function _p8(s) {
+          var p = (Math.random().toString(16)+"000000000").substr(2,8);
+          return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+        }
+        return _p8() + _p8(true) + _p8(true) + _p8();
+      },
+
+      empty: function() {
+        return '00000000-0000-0000-0000-000000000000';
+      }
+    };
+    return svc;
+  });
+
 
   pressurizeApp.config(['$routeProvider',
     function($routeProvider) {
@@ -19,7 +39,7 @@
           redirectTo: '/projects/'
         });
     }]);
- 
+
   pressurizeApp.service('sharedProperties', function () {
     var projects = (localStorage.getItem('projects')!==null) ? JSON.parse(localStorage.getItem('projects')) : [];
     var time_records = (localStorage.getItem('time_records')!==null) ? JSON.parse(localStorage.getItem('time_records')) : [];
@@ -53,7 +73,7 @@
       },
       getTimeRecords: function() {
         time_records = _.filter(time_records, function(p) {
-          if(typeof p.name != 'undefined') {
+          if(typeof p.hours != 'undefined') {
             return p;
           }
         });
@@ -80,6 +100,18 @@
   pressurizeApp.controller('ProjectsListController', ["$scope", "$http", "sharedProperties",
     function ($scope, $http, sharedProperties) {
       $scope.projects = sharedProperties.getProjects();
+
+      $scope.getProjectAssoicatedTimeRecords = function(id) {
+        var time_records = _.where(
+          sharedProperties.getTimeRecords(),
+          { project_number:  id }
+        );
+        if(!_.isArray(time_records)) {
+          return [time_records];
+        }
+        return time_records;
+      };
+
       $scope.init = function() {
 
         var checkContents = setInterval(function() {
@@ -94,8 +126,8 @@
     }
   ]);
 
-  pressurizeApp.controller('TimeRecordsController', ["$scope", "$http", "sharedProperties", "$compile",
-    function ($scope, $http, sharedProperties, $compile) {
+  pressurizeApp.controller('TimeRecordsController', ["$scope", "$http", "sharedProperties",
+    function ($scope, $http, sharedProperties) {
       $scope.time_record = {};
 
       $scope.timeRecordSubmit = function(is_valid) {
@@ -105,14 +137,14 @@
         var time_record = new TimeRecord($scope.time_record);
         var project = _.findWhere(
           sharedProperties.getProjects(),
-          { '$$hashKey': $scope.time_record.project_number }
+          { 'project_number': $scope.time_record.project_number }
         );
         // todo: refactor/extract all the below
         project.total_used = parseInt(project.total_used) + parseInt(time_record.hours);
         var all_projects = sharedProperties.getProjects();
        
         all_projects = _.map(function(p) {
-          if(p.$$hashKey == time_record.project_number) {
+          if(p.project_number == time_record.project_number) {
             return p;
           }
         });
@@ -128,10 +160,11 @@
     }
   ]);
 
-  pressurizeApp.controller('ProjectCreateController', ["$scope", "$http", "sharedProperties",
-    function ($scope, $http, sharedProperties) {
+  pressurizeApp.controller('ProjectCreateController', ["$scope", "$http", "sharedProperties", "uuid",
+    function ($scope, $http, sharedProperties, uuid) {
 
       $scope.project = {};
+
       $scope.submitForm = function(is_valid) {
         
         if(!is_valid) {
@@ -140,6 +173,7 @@
 
         sharedProperties.setProjects({
           name: $scope.project.name,
+          project_number: uuid.new(),
           description: $scope.project.description,
           dev_hours: $scope.project.dev_hours,
           total_used: $scope.project.total_used,
